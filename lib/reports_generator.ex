4608508bb54c8c30ddf25a9eb419eb42758e7b71
Lucas Stellet <lustepe@gmail.com>
2021-08-27 14:25:13 -0300

    up

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,18 +1,66 @@
 defmodule ReportsGenerator do
-  @moduledoc """
-  Documentation for `ReportsGenerator`.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
+  alias ReportsGenerator.Parser
 
-  ## Examples
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "hambúrguer",
+    "esfirra",
+    "pizza",
+    "prato_feito",
+    "sushi",
+    "pastel"
+  ]
 
-      iex> ReportsGenerator.hello()
-      :world
+  @options ["foods", "users"]
 
-  """
-  def hello do
-    :world
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), &sum_values/2)
   end
+
+  def build_from_many(filenames) do
+    filenames
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+  end
+
+  def fetch_higher_cost(report, option) when option in @options do
+    {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
+  end
+
+  def fetch_higher_cost(_report, _option), do: {:error, "Invalid option"}
+
+  defp report_acc do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    build_reports(foods, users)
+  end
+
+  defp sum_values([id, foodname, price], %{"users" => users, "foods" => foods} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, foodname, foods[foodname] + 1)
+
+    %{report | "users" => users, "foods" => foods}
+  end
+
+  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{
+         "foods" => foods2,
+         "users" => users2
+       }) do
+    foods = merge_maps(foods1, foods2)
+    users = merge_maps(users1, users2)
+
+    build_reports(foods, users)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+  end
+
+  defp build_reports(foods, users), do: %{"foods" => foods, "users" => users}
 end
--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -1,8 +1,96 @@
 defmodule ReportsGeneratorTest do
-  use ExUnit.Case
-  doctest ReportsGenerator
+  @moduledoc false
 
-  test "greets the world" do
-    assert ReportsGenerator.hello() == :world
+  use ExUnit.Case
+
+  describe "build/1" do
+    setup do
+      {:ok, report: ReportsGenerator.build("report_test.csv")}
+    end
+
+    test "builds a report from a csv file", %{report: response} do
+      expected_response = %{
+        "foods" => %{
+          "açaí" => 1,
+          "churrasco" => 2,
+          "esfirra" => 3,
+          "hambúrguer" => 2,
+          "pastel" => 0,
+          "pizza" => 2,
+          "prato_feito" => 0,
+          "sushi" => 0
+        },
+        "users" => %{
+          "1" => 48,
+          "10" => 36,
+          "11" => 0,
+          "12" => 0,
+          "13" => 0,
+          "14" => 0,
+          "15" => 0,
+          "16" => 0,
+          "17" => 0,
+          "18" => 0,
+          "19" => 0,
+          "2" => 45,
+          "20" => 0,
+          "21" => 0,
+          "22" => 0,
+          "23" => 0,
+          "24" => 0,
+          "25" => 0,
+          "26" => 0,
+          "27" => 0,
+          "28" => 0,
+          "29" => 0,
+          "3" => 31,
+          "30" => 0,
+          "4" => 42,
+          "5" => 49,
+          "6" => 18,
+          "7" => 27,
+          "8" => 25,
+          "9" => 24
+        }
+      }
+
+      assert response == expected_response
+    end
+  end
+
+  describe "fetch_higher_cost/1" do
+    setup do
+      {:ok, report: ReportsGenerator.build("report_test.csv")}
+    end
+
+    test "fetchs the higher cost from an users report as option", %{report: report} do
+      response =
+        report
+        |> ReportsGenerator.fetch_higher_cost("users")
+
+      expected_response = {:ok, {"5", 49}}
+
+      assert response == expected_response
+    end
+
+    test "fetchs the higher cost from an foods report as option", %{report: report} do
+      response =
+        report
+        |> ReportsGenerator.fetch_higher_cost("foods")
+
+      expected_response = {:ok, {"esfirra", 3}}
+
+      assert response == expected_response
+    end
+
+    test "receives an error tuple when pass an unknown report option", %{report: report} do
+      response =
+        report
+        |> ReportsGenerator.fetch_higher_cost("banana")
+
+      expected_response = {:error, "Invalid option"}
+
+      assert response == expected_response
+    end
   end
 end
